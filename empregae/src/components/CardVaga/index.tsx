aceb42986a6c5c5a7422fcdd4f2b161bbff0f1c7
Andreina Melo <andreinanovaes@gmail.com>
2024-11-30 12:02:52 -0300

    feat: adicao da api de vagas

--- empregae/src/components/CardVaga/index.tsx
+++ empregae/src/components/CardVaga/index.tsx
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import { useNavigation } from '@react-navigation/native';
-import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-
-import { Container, Conteudo, BotaoAbrir, Titulo, Status } from './styles';
-import { Feather } from '@expo/vector-icons';
-
-import { RootStackParamList } from '@/src/utils/types';
-
-interface Data{
-    id: number;
-    titulo: string;
-    status: string;
-}
-
-
-type Props = NativeStackScreenProps<RootStackParamList>;
-
-export default function Vaga({id, titulo, status}: Data) {
-    const navigation = useNavigation<Props['navigation']>();
-    
-    return (
-        <Container onPress={() => navigation.navigate('Detalhes')}>
-            <Conteudo>
-                <Titulo numberOfLines={1}>{titulo}</Titulo>
-                <Status numberOfLines={1}>Status: {status}</Status>
-            </Conteudo>
-            <BotaoAbrir>
-                <Feather name="maximize-2" size={32} color={'#FFFFFF'} />
-            </BotaoAbrir>
-        </Container>
-    );
-=======
 import { useNavigation } from '@react-navigation/native';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
@@ -62,5 +29,4 @@ export default function Vaga({id, titulo, status}: Data) {
             </BotaoAbrir>
         </Container>
     );
->>>>>>> dcb2ea737caca0963c4000eab073eede393e0e31
 }
