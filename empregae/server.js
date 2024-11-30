b5d9005d56c0556318f632760a0994094213f545
Estêvão Viana Queiroz <101609143+Txtravos@users.noreply.github.com>
2024-11-30 23:45:41 +0000

    configuracao server.js

--- empregae/server.js
+++ empregae/server.js
@@ -7,6 +7,10 @@ const vagasRoutes = require('./routes/vagas');
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.send('Servidor está funcionando!');
+});
+
 // sincronizar BD
 sequelize.sync().then(() => {
     console.log('Database synchronized');
